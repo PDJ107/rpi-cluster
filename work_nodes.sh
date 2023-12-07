47e59c77d2323ae3732a4fa8b81592e046a3391f
박동주 <cxz541@gmail.com>
2023-12-07 18:08:19 +0900

    Fix: join 수정

--- work_nodes.sh
+++ work_nodes.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 # config for work_nodes only
-sudo kubeadm join --token 123456.1234567890123456 \ 
---discovery-token-unsafe-skip-ca-verification 192.168.10.11:6443
+sudo kubeadm join --token 123456.1234567890123456 --discovery-token-unsafe-skip-ca-verification 192.168.10.11:6443
